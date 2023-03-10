545fb527ece921fb0fc8d3ba95a32d693e13dd52
IjlalSultan <111721012+IjlalSultan@users.noreply.github.com>
2023-03-09 19:58:27 -0500

    updated sketch.js

--- sketch.js
+++ sketch.js
@@ -3,20 +3,26 @@
 
 // Video
 let video;
-
+let classifier;
+let label = "waiting...";
 // STEP 1: Load the model!
-
+function preload(){
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/p3BKNaoGT/");
+}
 function setup() {
-  createCanvas(640, 520);
+  createCanvas(300, 300);
   // Create the video
   video = createCapture(VIDEO);
   video.hide();
 
   // STEP 2: Start classifying
+  classifyImage();
 }
 
 // STEP 2 classify!
-
+function classifyImage(){
+  classifier.classify(video,gotResults);
+}
 function draw() {
   background(0);
   
@@ -24,7 +30,20 @@ function draw() {
   image(video, 0, 0);
 
   // STEP 4: Draw the label
+  textSize(32);
+  textAlign (CENTER,CENTER);
+  fill("red");
+  text(label,width/2,height-16);
 }
 
 
 // STEP 3: Get the classification!
+function gotResults(error,results){
+  if(error){
+    console.error(error);
+  }
+  else{
+    label = results[0].label;
+    classifyImage();
+  }
+}
